394cb57a82067264af9ab4425f18affbfd2c5eb0
neweagleraptor <neweagleraptorgithub@neweagle.net>
2021-06-14 13:36:43 -0400

    Imported upstream version '1.1.0' of 'upstream'

--- raptor_pdu/include/raptor_pdu/raptor_pdu.hpp
+++ raptor_pdu/include/raptor_pdu/raptor_pdu.hpp
@@ -26,6 +26,11 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
+/** \brief This file defines the raptor_pdu class.
+ * \copyright Copyright 2021 New Eagle LLC
+ * \file raptor_pdu.hpp
+ */
+
 #ifndef RAPTOR_PDU__RAPTOR_PDU_HPP_
 #define RAPTOR_PDU__RAPTOR_PDU_HPP_
 
@@ -40,26 +45,36 @@
 #include <std_msgs/msg/empty.hpp>
 #include <std_msgs/msg/string.hpp>
 
-#include <raptor_can_dbc_parser/Dbc.hpp>
-#include <raptor_can_dbc_parser/DbcBuilder.hpp>
-#include <raptor_can_dbc_parser/DbcMessage.hpp>
-#include <raptor_can_dbc_parser/DbcSignal.hpp>
-#include <raptor_can_dbc_parser/LineParser.hpp>
+#include <can_dbc_parser/Dbc.hpp>
+#include <can_dbc_parser/DbcBuilder.hpp>
+#include <can_dbc_parser/DbcMessage.hpp>
+#include <can_dbc_parser/DbcSignal.hpp>
+#include <can_dbc_parser/LineParser.hpp>
 
 #include <string>
 
+using can_msgs::msg::Frame;
+using raptor_pdu_msgs::msg::FuseReport;
+using raptor_pdu_msgs::msg::RelayCommand;
+using raptor_pdu_msgs::msg::RelayReport;
+
 namespace NewEagle
 {
+/** \brief Class for interacting with the PDU */
 class raptor_pdu : public rclcpp::Node
 {
-  enum
+  /** \brief Enumeration of message base addresses */
+  enum ListAddresses
   {
-    RELAY_STATUS_BASE_ADDR = 0x18ffa100,
-    FUSE_STATUS_BASE_ADDR = 0x18ffa000,
-    RELAY_COMMAND_BASE_ADDR = 0x18ef0000
+    RELAY_STATUS_BASE_ADDR = 0x18ffa100,  /**< Relay status message base address */
+    FUSE_STATUS_BASE_ADDR = 0x18ffa000,   /**< Fuse status message base address */
+    RELAY_COMMAND_BASE_ADDR = 0x18ef0000  /**< Relay command message base address */
   };
 
 public:
+/** \brief Default constructor.
+ * \param[in] options The options for this node.
+ */
   explicit raptor_pdu(const rclcpp::NodeOptions & options);
 
 private:
@@ -76,17 +91,24 @@ private:
   NewEagle::Dbc pduDbc_;
   std::string pduFile_;
 
-  void recvCAN(const can_msgs::msg::Frame::SharedPtr msg);
-  void recvRelayCmd(const raptor_pdu_msgs::msg::RelayCommand::SharedPtr msg);
+/** \brief Convert reports received over CAN into ROS messages.
+ * \param[in] msg The message received over CAN.
+ */
+  void recvCAN(const Frame::SharedPtr msg);
+
+/** \brief Send out a Relay Command over CAN
+ * \param[in] msg The RelayCommand received as a ROS message.
+ */
+  void recvRelayCmd(const RelayCommand::SharedPtr msg);
 
   // Subscribed topics
-  rclcpp::Subscription<can_msgs::msg::Frame>::SharedPtr sub_can_;
-  rclcpp::Subscription<raptor_pdu_msgs::msg::RelayCommand>::SharedPtr sub_relay_cmd_;
+  rclcpp::Subscription<Frame>::SharedPtr sub_can_;
+  rclcpp::Subscription<RelayCommand>::SharedPtr sub_relay_cmd_;
 
   // Published topics
-  rclcpp::Publisher<can_msgs::msg::Frame>::SharedPtr pub_can_;
-  rclcpp::Publisher<raptor_pdu_msgs::msg::FuseReport>::SharedPtr fuse_report_pub_;
-  rclcpp::Publisher<raptor_pdu_msgs::msg::RelayReport>::SharedPtr relay_report_pub_;
+  rclcpp::Publisher<Frame>::SharedPtr pub_can_;
+  rclcpp::Publisher<FuseReport>::SharedPtr fuse_report_pub_;
+  rclcpp::Publisher<RelayReport>::SharedPtr relay_report_pub_;
 };
 }  // namespace NewEagle
 
